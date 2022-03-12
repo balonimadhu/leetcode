669f0e62cd7e1c67d1eb666bb4b325966591e3c5
balonimadhu <balonimadhu@gmail.com>
2022-03-12 19:15:28 +0530

    file pushed

---
+++ binarysearch.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        first_idx=0
+        last_idx=len(nums)-1
+        while last_idx>=first_idx:
+            mid=(first_idx+last_idx)//2
+            if target== nums[mid]:
+                return mid
+                break
+            elif target<nums[mid]:
+                last_idx=mid-1
+            else:
+                first_idx=mid+1
+        if first_idx>last_idx:
+            return -
+        
