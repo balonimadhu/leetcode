365447c597e0a00506eab339c424975c86ea4e42
balonimadhu <balonimadhu@gmail.com>
2022-03-18 21:42:30 +0530

    file has been pushed

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if(x<0):
+           return False
+        rev=0
+        value=x
+        while(value>0):
+            rem=value%10
+            value=value//10
+            rev=rev*10+rem
+        return rev==x
+        
