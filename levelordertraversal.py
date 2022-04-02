f3b4480ffda9e39326d5c99a32c1cc6fc5674a96
balonimadhu <balonimadhu@gmail.com>
2022-04-02 14:11:44 +0530

    code pushed

---
+++ levelordertraversal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q, result = deque(), []
+        if root:
+            q.append(root)
+        while len(q):
+            level = []
+            for _ in range(len(q)):
+                x = q.popleft()
+                level.append(x.val)
+                if x.left:
+                    q.append(x.left)
+                if x.right:
+                    q.append(x.right)
+            result.append(level)
+        return result
